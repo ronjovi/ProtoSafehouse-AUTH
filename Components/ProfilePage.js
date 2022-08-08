a4404a3420ff42607fd41390b3dc1efbe4dc4687
Renzo Basurco <darkgalaxy735@gmail.com>
2022-08-08 11:52:30 -0700

    MVP Profile Page

--- Components/ProfilePage.js
+++ Components/ProfilePage.js
@@ -6,9 +6,9 @@ const ProfilePage = ({ navigation }) => {
         <View style={styles.container}>
             <SafeAreaView>
                 <View style={styles.NFTView}>
-                    <Image source={require('../assets/Clean_Rat.png')} style={[styles.NFTBadge, { marginTop: 110, alignSelf: 'flex-start' }]} />
-                    <Image source={require('../assets/Coolcat.png')} style={[styles.NFTBadge, { marginTop: 110, alignSelf: 'flex-end' }]} />
-                    <Image source={require('../assets/Cat_stronaut.png')} style={[styles.NFTBadge, { alignSelf: 'center', marginTop: 180, }]} />
+                    <Image source={require('../assets/Clean_Rat.png')} style={[styles.NFTBadge, { marginTop: 80, alignSelf: 'flex-start' }]} />
+                    <Image source={require('../assets/Coolcat.png')} style={[styles.NFTBadge, { marginTop: 80, alignSelf: 'flex-end' }]} />
+                    <Image source={require('../assets/Cat_stronaut.png')} style={[styles.NFTBadge, { alignSelf: 'center', marginTop: 150, }]} />
                 </View>
                 <View>
                     <TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate('AnimalList')}>
@@ -18,6 +18,9 @@ const ProfilePage = ({ navigation }) => {
                 <View style={styles.profileView}>
                     {/* <ScrollView> */}
                     <FlatList
+                        scrollEnabled={false}
+                        // style = {{backgroundColor : 'red'}}
+                        // style = {{marginBottom: 50}}
                         numColumns={2}
                         data={[
                             { username: 'Rena Råttor', contributed: '$4600', bio: "Who said rats and cats can't be friends?" }
@@ -36,67 +39,109 @@ const ProfilePage = ({ navigation }) => {
 
                         )}
                     />
+
                     <View>
-                    <FlatList
-                    style={styles.CatsAdopted}
-                    scrollEnabled={false}
-                    columnWrapperStyle={styles.row}
-                    numColumns={3}
-                    ListHeaderComponent={
-                        <>
-                        <Text style={{fontSize: 20, fontFamily: 'lightK2D', marginLeft: 20, marginBottom: 10,}}>Cats Adopted</Text>
-                        <View style={{height: 2, width: '90%', backgroundColor: '#545454', alignSelf: 'center', top: 105}}></View>
-                        </>
-                    }
-                    data={[
-                        {image: 'https://d.newsweek.com/en/full/1980820/curled-sleeping-cat.jpg'},
-                        {image: 'https://d.newsweek.com/en/full/1980820/curled-sleeping-cat.jpg'},
-                        {image: 'https://d.newsweek.com/en/full/1980820/curled-sleeping-cat.jpg'},
-                    ]}
-                    renderItem={({ item }) => (
-                        <View>
-                            <Image source={{ uri: item.image }} style={{height: 90, width: 90, borderRadius: 15, borderWidth: 1}}/>
-                        </View>
-                    )}
-                    ListFooterComponent={
-                        <>
-                        <TouchableOpacity style={{bottom: 90, left: 360, backgroundColor: '#62BA75', justifyContent: 'center', width: 50, height: 90, borderRadius: 10,}}>
-                            <Text style={{color: 'white', alignSelf: 'center', fontFamily: 'K2D', fontSize: 16, width: 34.5}}>View All</Text>
-                        </TouchableOpacity>
-                        </>
-                    }
-                    />
+                        <FlatList
+                            style={styles.CatsAdopted}
+                            scrollEnabled={false}
+                            columnWrapperStyle={styles.row}
+                            numColumns={3}
+                            ListHeaderComponent={
+                                <>
+                                    <Text style={{ fontSize: 18, fontFamily: 'K2D', marginLeft: 20, marginBottom: 5, }}>Cats Adopted</Text>
+                                    <View style={{ height: 2, width: '90%', backgroundColor: '#545454', alignSelf: 'center', top: 100 }}></View>
+                                </>
+                            }
+                            data={[
+                                { image: 'https://d.newsweek.com/en/full/1980820/curled-sleeping-cat.jpg' },
+                                { image: 'https://media.npr.org/assets/img/2021/08/11/gettyimages-1279899488_wide-f3860ceb0ef19643c335cb34df3fa1de166e2761-s1600-c85.webp' },
+                                { image: 'https://www.incimages.com/uploaded_files/image/1920x1080/getty_513189787_110007.jpg' },
+                            ]}
+                            renderItem={({ item }) => (
+                                <View>
+                                    <Image source={{ uri: item.image }} style={{ height: 81, width: 81, borderRadius: 15, borderWidth: 1 }} />
+                                    <Image source={require('../assets/GreenIcon.png')} style={{ height: 13, width: 13, bottom: 17, left: 63, }} />
+                                </View>
+                            )}
+                            ListFooterComponent={
+                                <>
+                                    <TouchableOpacity style={{ bottom: 93, left: 360, backgroundColor: '#62BA75', display: 'flex', justifyContent: 'center', width: 35, height: 81, borderRadius: 10, }}>
+                                        <Text style={{ color: 'white', textAlign: 'center', fontFamily: 'K2D', fontSize: 12, width: 34.5 }}>View All</Text>
+                                    </TouchableOpacity>
+                                </>
+                            }
+                        />
                     </View>
-                    <View style={{bottom: 60}}>
-                    <FlatList
-                    style={styles.CatsAdopted}
-                    scrollEnabled={false}
-                    columnWrapperStyle={styles.row}
-                    numColumns={3}
-                    ListHeaderComponent={
-                        <>
-                        <Text style={{fontSize: 20, fontFamily: 'lightK2D', marginLeft: 20, marginBottom: 10,}}>Successful Support</Text>
-                        <View style={{height: 2, width: '90%', backgroundColor: '#545454', alignSelf: 'center', top: 105}}></View>
-                        </>
-                    }
-                    data={[
-                        {image: 'https://d.newsweek.com/en/full/1980820/curled-sleeping-cat.jpg'},
-                        {image: 'https://d.newsweek.com/en/full/1980820/curled-sleeping-cat.jpg'},
-                        {image: 'https://d.newsweek.com/en/full/1980820/curled-sleeping-cat.jpg'},
-                    ]}
-                    renderItem={({ item }) => (
-                        <View>
-                            <Image source={{ uri: item.image }} style={{height: 90, width: 90, borderRadius: 15, borderWidth: 1}}/>
-                        </View>
-                    )}
-                    ListFooterComponent={
-                        <>
-                        <TouchableOpacity style={{bottom: 90, left: 360, backgroundColor: '#62BA75', justifyContent: 'center', width: 50, height: 90, borderRadius: 10,}}>
-                            <Text style={{color: 'white', alignSelf: 'center', fontFamily: 'K2D', fontSize: 16, width: 34.5}}>View All</Text>
-                        </TouchableOpacity>
-                        </>
-                    }
-                    />
+                    <View style={{ bottom: 60 }}>
+                        <FlatList
+                            style={styles.CatsAdopted}
+                            scrollEnabled={false}
+                            columnWrapperStyle={styles.row}
+                            numColumns={3}
+                            ListHeaderComponent={
+                                <>
+                                    <Text style={{ fontSize: 18, fontFamily: 'K2D', marginLeft: 20, marginBottom: 5, }}>Successful Support</Text>
+                                    <View style={{ height: 2, width: '90%', backgroundColor: '#545454', alignSelf: 'center', top: 100 }}></View>
+                                </>
+                            }
+                            data={[
+                                { image: 'https://imgix.bustle.com/scary-mommy/2019/09/GettyImages-146582583-min-1.jpg' },
+                                { image: 'https://www.scottishspca.org/sites/default/files/styles/full_content/public/2019-09/Buying%20a%20cat%201000x600.jpeg' },
+                                { image: 'https://data.whicdn.com/images/347673612/original.jpg' },
+                            ]}
+                            renderItem={({ item }) => (
+                                <View>
+                                    <Image source={{ uri: item.image }} style={{ height: 81, width: 81, borderRadius: 15, borderWidth: 1 }} />
+                                    <Image source={require('../assets/GreenIcon.png')} style={{ height: 13, width: 13, bottom: 17, left: 63, }} />
+                                </View>
+                            )}
+                            ListFooterComponent={
+                                <>
+                                    <TouchableOpacity style={{ bottom: 93, left: 360, backgroundColor: '#62BA75', display: 'flex', justifyContent: 'center', width: 35, height: 81, borderRadius: 10, }}>
+                                        <Text style={{ color: 'white', textAlign: 'center', fontFamily: 'K2D', fontSize: 12, width: 34.5 }}>View All</Text>
+                                    </TouchableOpacity>
+                                </>
+                            }
+                        />
+                    </View>
+
+                    <View style = {{position: 'absolute', top: 475}}>
+                        <FlatList
+                            style={styles.CatsAdopted}
+                            scrollEnabled={false}
+                            columnWrapperStyle={styles.row}
+                            numColumns={3}
+
+                            ListHeaderComponent={
+                                <>
+                                    <Text style={{ fontSize: 18, fontFamily: 'K2D', marginLeft: 20, marginBottom: 10, }}>Badge Showcase</Text>
+                                </>
+                            }
+
+                            data={[
+                                { image: require('../assets/Cat_stronaut.png'), },
+                                { image: require('../assets/Clean_Rat.png'), },
+                                { image: require('../assets/Coolcat.png'), },
+                            ]}
+
+                            // renderItem={({ item }) => (
+                            //     <View>
+                            //         <Image source={{ uri: item.image }} style={{height: 81, width: 81, borderRadius: 15, borderWidth: 1}}/>
+                            //     </View>
+                            // )}
+                            renderItem={({ item }) => (
+                                <View>
+                                    <Image source={item.image} style={{ height: 85, width: 85, borderRadius: 100, borderWidth: 1 }} />
+                                </View>
+                            )}
+                            ListFooterComponent={
+                                <>
+                                <TouchableOpacity style={{ bottom: 85, marginLeft: 360, backgroundColor: '#62BA75', justifyContent: 'center', width: 35, height: 81, borderRadius: 10,}}>
+                                    <Text style={{color: 'white', textAlign: 'center', fontFamily: 'K2D', fontSize: 12, width: 34.5}}>View All</Text>
+                                </TouchableOpacity>
+                                </>
+                            }
+                        />
                     </View>
                     {/* <View style={{bottom: 120,}}>
                     <FlatList
@@ -106,7 +151,7 @@ const ProfilePage = ({ navigation }) => {
                     numColumns={3}
                     ListHeaderComponent={
                         <>
-                        <Text style={{fontSize: 20, fontFamily: 'lightK2D', marginLeft: 20, marginBottom: 10,}}>Badge Showcase</Text>
+                        <Text style={{fontSize: 18, fontFamily: 'K2D', marginLeft: 20, marginBottom: 10,}}>Badge Showcase</Text>
                         <View style={{height: 2, width: '90%', backgroundColor: '#545454', alignSelf: 'center', top: 105}}></View>
                         </>
                     }
@@ -143,7 +188,7 @@ const styles = StyleSheet.create({
     },
     row: {
         justifyContent: "space-between",
-        width: 320,
+        width: 310,
         marginLeft: 20,
     },
     userAndConBox: {
@@ -157,12 +202,14 @@ const styles = StyleSheet.create({
         bottom: 55,
     },
     CatsAdopted: {
-        backgroundColor: 'lightgray'
+        // backgroundColor: 'lightgray'
+        // marginBottom: 10
+        bottom: 70
     },
     NFTBadge: {
         position: 'absolute',
-        height: 200,
-        width: 200,
+        height: 181,
+        width: 181,
         borderWidth: 2,
         borderRadius: 100,
     },
@@ -188,7 +235,7 @@ const styles = StyleSheet.create({
         position: 'absolute',
         backgroundColor: 'white',
         height: 600,
-        marginTop: 390,
+        marginTop: 330,
         borderRadius: 10,
         width: '100%',
     },
